5a99b8192a63b242dad75d322e611731f47e71d2
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-06-24 09:37:38 -0300

    init project

---
+++ src/main/java/com/java/patterns/java_patterns/JavaPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.java.patterns.java_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPatternsApplication.class, args);
+	}
+
+}

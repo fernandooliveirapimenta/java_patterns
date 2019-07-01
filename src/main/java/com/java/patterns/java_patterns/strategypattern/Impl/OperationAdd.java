396202d433e1623c9758153a48529a741f8500da
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-07-01 09:08:52 -0300

    Strategy Pattern

---
+++ src/main/java/com/java/patterns/java_patterns/strategypattern/Impl/OperationAdd.java
@@ -0,0 +1,9 @@
+package com.java.patterns.java_patterns.strategypattern.Impl;
+
+public class OperationAdd implements Strategy {
+
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/main/java/com/java/patterns/java_patterns/strategypattern/Impl/Strategy.java
@@ -0,0 +1,5 @@
+package com.java.patterns.java_patterns.strategypattern.Impl;
+
+public interface Strategy {
+    int doOperation(int num1, int num2);
+}
